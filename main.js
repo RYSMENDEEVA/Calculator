c8df484cea4d19d42b2e0f4505f99a2a838fc63d
Adinai <RYSMENDEEVAADINAI@GMAIL.COM>
2023-10-20 20:08:34 +0600

    add: styles

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 let display = document.querySelector(".calculator__display");
 
-let buttons = Array.from(document.querySelectorAll(".btn"));
+let buttons = document.querySelector(".calculator__buttons");
 
-buttons.map((btn) => {
-  btn.addEventListener("click", (e) => {
-    switch (e.target.innerText) {
-      case "AC":
-        display.innerText = "0";
-        break;
-      case "=":
-        try {
-          display.innerText = eval(display.innerText);
-        } catch (e) {
-          display.innerText = "Error!";
-        }
-        break;
-      case "+/-":
-        display.innerText = "-";
-        break;
-      case "%":
-        let passedText = display.innerText + "/100";
-        display.innerText = eval(passedText);
-        break;
+buttons.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("btn")) return;
 
-      default:
-        if (display.innerText === "0" && e.target.innerText !== ".") {
-          display.innerText = e.target.innerText;
-        } else {
-          display.innerText += e.target.innerText;
-        }
-    }
-  });
+  switch (e.target.innerText) {
+    case "AC":
+      display.innerText = "0";
+      break;
+    case "=":
+      try {
+        display.innerText = eval(display.innerText);
+      } catch (e) {
+        display.innerText = "Error!";
+      }
+      break;
+    case "+/-":
+      display.innerText = "-";
+      break;
+    case "%":
+      let passedText = display.innerText + "/100";
+      display.innerText = eval(passedText);
+      break;
+
+    default:
+      if (display.innerText === "0" && e.target.innerText !== ".") {
+        display.innerText = e.target.innerText;
+      } else {
+        display.innerText += e.target.innerText;
+      }
+  }
 });
